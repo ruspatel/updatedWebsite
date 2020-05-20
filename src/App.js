ae86aa719c1d491516416a158a46a601c7cbed5b
Rushil Patel <ra8patel@edu.uwaterloo.ca>
2020-05-20 12:51:00 -0400

    created links in header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Nav from './Components/nav.js';
+
+import Blog from './Components/blog.js';
+import About from './Components/about.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a href="./bar.js">link</a>
-      </header>
+     <Router>
+     <div className="App">
+       <Nav />
+       <Route path="/about" component={About} />
+       <Route path="/blog" component={Blog}/>
+
     </div>
+    </Router>
+
   );
 }
 
---
+++ src/Components/nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+
+
+function nav(){
+    return(
+        <div className="navbar">
+                <Link className="header-link" to="/about"><h1>About</h1></Link>
+                <Link className="header-link" to="/blog"><h1>Blog</h1></Link>
+                <Link className="header-link" to="/projects"><h1>Projects</h1></Link>
+                <Link className="header-link" to="/experience"><h1>Experience</h1></Link>
+                <h1>Resume</h1>
+        </div>
+    );
+}
+
+export default nav;
